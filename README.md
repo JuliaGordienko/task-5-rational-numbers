b8ad8d8689b7a639d0bd53f79e0297dce0fc9878
sergeniys <sergeykartsev2016@gmail.com>
2025-11-16 22:45:04 +0300

    firstcomm

---
+++ README.md
@@ -0,0 +1,18 @@
+## Проект task 5: rational numbers
+Требуется Python 3.12 или выше.
+
+### Установка и запуск
+Установка (Linux / macOS)
+python3 -m venv .venv
+source .venv/bin/activate\
+
+### Установка (Windows Powershell)
+python -m venv .venv
+.venv\Scripts\Activate.ps1\
+
+### Зaпycк
+python main.py
+
+Задание
+Рациональные числа являются счетным множеством.
+Реализовать генератор (очевидно, неограниченный), выдающий все рациональные числа по одному разу.
---
+++ main.py
@@ -0,0 +1,33 @@
+from fractions import Fraction
+from math import gcd
+from itertools import islice
+
+def rationals():
+    """Генератор, перечисляющий все рациональные числа по одному разу.
+    Представление: приводимая дробь p/q с q>0 и gcd(|p|,q)=1.
+    Перебираем пары (p,q) в порядке возрастания |p|+q (диагоналями).
+    """
+    k = 0
+    while True:
+        for q in range(1, k + 2):
+            p_abs = k - q
+            if p_abs < 0:
+                continue
+            if p_abs == 0:
+                p_vals = (0,)
+            else:
+                p_vals = (-p_abs, p_abs)
+            for p in p_vals:
+                if abs(p) == 0:
+                    if q != 1:  # 0 допускаем только как 0/1
+                        continue
+                else:
+                    if gcd(abs(p), q) != 1: # наибольший общий делитель чисел p и q ->  Если дробь p/q сократимая, то пропускаем её
+                        continue
+                yield Fraction(p, q)
+        k += 1
+
+# Пример: вывести первые 80 рациональных
+gen = rationals()
+for i, r in enumerate(islice(gen, 80), 1):
+    print(i, r)
